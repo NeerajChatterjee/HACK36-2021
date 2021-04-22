a582b51589db21d00a5ff7fbb99af8dea2034d52
NeerajChatterjee <73419394+NeerajChatterjee@users.noreply.github.com>
2021-04-22 15:26:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,53 @@
+# Pradhanmantri-Manoranjan-Yojna
+
+<h1 align="center">PRADHANMANTRI MANORANJAN YOJNA</h1>
+<p align="center">
+</p>
+
+<a href="https://hack36.com"> <img src="http://bit.ly/BuiltAtHack36" height=20px> </a>
+
+<b>Check it out :fire:<a href=https://silkie261001.github.io/Pradhanmantri-Manoranjan-Yojna/ target="blank">HERE</a></b>:fire:.
+
+## Introduction:
+
+Team Hypertext Assassins' main motto is to present a platform to the audience where they can enjoy and get knowledge regarding COVID-19. So, we made a website that focuses on every aspect like entertainment, vaccine registration form, awareness through SOP's, COVID-19 cases tracker across the world.
+
+## Technology Stack used:
+<img src="https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white"/> <img src="https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white"/>  <img src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
+  
+## Contributors:
+
+Team Name: Hypertext Assassin's
+
+* [Silkie Agarwal](https://github.com/silkie261001)
+* [Kartikey Gupta](https://github.com/kartikey2991)
+* [Neeraj Chatterjee](https://github.com/NeerajChatterjee)
+* [Shivanand](https://github.com/pinnacle20)
+
+## Briefing powerpoint presentation
+
+https://github.com/silkie261001/Pradhanmantri-Manoranjan-Yojna/blob/main/HYPERTEXT%20ASSASSINS.pptx (Please view raw file)
+
+## Screenshots
+
+<img src="https://github.com/silkie261001/Pradhanmantri-Manoranjan-Yojna/blob/main/screenshot1.jpg"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
+<img src="https://github.com/silkie261001/Pradhanmantri-Manoranjan-Yojna/blob/main/screenshot2.jpg"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
+<img src="https://github.com/silkie261001/Pradhanmantri-Manoranjan-Yojna/blob/main/screenshot3.jpg"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
+<img src="https://github.com/silkie261001/Pradhanmantri-Manoranjan-Yojna/blob/main/screenshot4.jpg"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
+<img src="https://github.com/silkie261001/Pradhanmantri-Manoranjan-Yojna/blob/main/screenshot5.jpg"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
+     
+
+
+### Made at:
+<a href="https://hack36.com"> <img src="http://bit.ly/BuiltAtHack36" height=20px> </a>
+
---
+++ tracker_app.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+  $("#query-form").submit(function(event) { performSearch(event); });
+});
+
+
+var pat,flag=0;
+function formatSearchResults(jsonResults) {
+
+  var jsonObject = jsonResults;
+ 
+      $("#search-results-heading").text("Search Results");
+    var formatedText = "";
+
+    jsonObject.Countries.forEach(
+      function(item, index) {
+        
+        if(item.Country.toLowerCase()==pat.toLowerCase()){
+        var thumbnail = item.NewConfirmed;
+       formatedText += "<div class='dish-ingredients-div'><h3>TotalConfirmed: " + item.TotalConfirmed + "<h3></div>";
+       formatedText += "<div class='dish-ingredients-div'><h3>NewDeaths: " + item.NewDeaths + "<h3></div>";
+       formatedText += "<div class='dish-ingredients-div'><h3>NewConfirmed: " + item.NewConfirmed + "<h3></div>";
+       formatedText += "<div class='dish-ingredients-div'><h3>NewRecovered: " + item.NewRecovered + "<h3></div>";
+       flag=1;
+        return;
+      }
+    }
+    );
+
+$("#results").html(formatedText);
+  if(!flag){setNotFoundMessages();}
+
+}
+
+function performSearch(event) {
+  var request;
+  event.preventDefault();
+  if (request) {
+      request.abort();
+  }
+  var $form = $(this);
+  setFormDisabledProps(true);
+
+  $("#search-results-heading").text("Searching ...");
+  $("#results").text("");
+  request = $.ajax({
+      url:"https://api.covid19api.com/summary",
+      type: "GET",
+  });
+
+pat=$("#ingredients").val();
+  request.done(function (response,textStatus, jqXHR){
+    formatSearchResults(response);
+    console.log(pat)
+  });
+
+  request.fail(function (jqXHR, textStatus, errorThrown){
+      $("#search-results-heading").text("Sorry We Unable to fetch Covid Data.Try again.");
+      $("#results").text("");
+  });
+
+  request.always(function () {
+      setFormDisabledProps(false);
+  });
+
+}
+
+function resetResults() {
+  $("#search-results-heading").text("");
+  $("#results").text("");
+  flag=0;
+}
+
+
+function sanitizeInputs() {
+  var str = $("#ingredients").val();
+  str = str.replace(/[^a-zA-Z 0-9,]/gim, "");
+  str = str.trim();
+  $("#ingredients").val(str);
+}
+
+
+function setFormDisabledProps(statusToSet) {
+    document.getElementById("ingredients").disabled = statusToSet;
+    document.getElementById("resetButton").disabled = statusToSet;
+    document.getElementById("searchButton").disabled = statusToSet;
+}
+function setNotFoundMessages() {
+  $("#search-results-heading").text("Please Enter Correct Country Name e.g-India");
+  $("#results").text("");
+}
